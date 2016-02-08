2da10fb1921f9072dd957477fabed51536c155ba
umuraras <umuraras@gmail.com>
2016-02-08 17:59:43 +0200

    ceated prj

---
+++ WebApiClient/Views/Home/ApiService.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+var app = angular.module("ApiApp", []);
+
+app.controller("ApiController", function ($scope, $http) {
+    $http.post .get('data/posts.json').
+      success(function (data, status, headers, config) {
+          $scope.posts = data;
+      }).
+      error(function (data, status, headers, config) {
+          // log error
+      });
+});
